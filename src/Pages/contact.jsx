411157a9aef95637873d56869308f164baa02564
Callum Townley <cllmtownley@gmail.com>
2025-10-25 12:42:31 +0100

    added the ability for the form to send an email, added error and success popups

--- src/Pages/contact.jsx
+++ src/Pages/contact.jsx
@@ -1,28 +1,103 @@
 import ContactCircle from "../Components/ContactCircle";
+import { useState } from "react";
+import Swal from "sweetalert2";
 
 function Contact(){
+    const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: ""
+  });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleSubmit = async (event) => {
+    console.log("Submitting form data:", formData);
+    event.preventDefault();
+    setIsSubmitting(true);
+
+    try {
+         // Debugging line
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          access_key: "PUT YOUR ACCESS KEY HERE",
+          ...formData
+        }),
+      });
+
+      const result = await response.json();
+      if (result.success) {
+        Swal.fire({
+            title: "Success!",
+            text: "Message sent successfully!",
+            icon: "success",
+            background: "#26578F",
+            color: "#FFFFFF",
+            
+        });
+        setFormData({ name: "", email: "", message: "" });
+      } else {
+        Swal.fire({
+            title: "Oh no!",
+            text: "Something went wrong, please try again",
+            icon: "error"
+        });
+      }
+    } catch (error) {
+      Swal.fire({
+            title: "Oh no!",
+            text: "Something went wrong, please try again",
+            icon: "error"
+        });
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+
+
+
     return ( <div className="bg-[#0A2840] min-h-screen h-fit flex flex-col  justify-center items-center relative">
+        <form className="flex flex-col justify-center items-center" onSubmit={handleSubmit}>
             <div className="flex justify-evenly w-full items-center pt-20 max-w-[1920px]">
              <ContactCircle src={'/src/assets/Mail.svg'}>Stevenollerenshaw@gmail.com</ContactCircle>
                         {/*name and email text boxes*/}
+                        
                          <div className="flex flex-col items-center w-full max-w-[70%] md:max-w-[40%]  ">
                              <h1 className="text-[clamp(2rem,3.5vw,4rem)] font-poppins font-extrabold mb-8 bg-clip-text text-transparent bg-gradient-to-b from-white from-70% to-gray-400 text-center">CONTACT US</h1>
+                             
                             <div className="w-full mb-4">
-                                <label htmlFor="contact-name" className="block text-left text-white text-xl 2xl:text-2xl font-poppins mb-1 pl-1">Name:</label>
+                                <label htmlFor="name" className="block text-left text-white text-xl 2xl:text-2xl font-poppins mb-1 pl-1">Name:</label>
                                 <input
-                                    id="contact-name"
+                                    name="name"
+                                    id="name"
                                     type="text"
                                     placeholder="Name"
+                                    value={formData.name}
                                     className="w-full px-4 py-2 rounded-lg bg-white/80 text-black focus:outline-none focus:ring-2 focus:ring-blue-400"
+                                    required
+                                    onChange={handleChange}
                                 />
                             </div>
                             <div className="w-full mb-4">
-                                <label htmlFor="contact-email" className="block text-left text-white text-xl 2xl:text-2xl font-poppins  mb-1 pl-1">Email Address:</label>
+                                <label htmlFor="email" className="block text-left text-white text-xl 2xl:text-2xl font-poppins  mb-1 pl-1">Email Address:</label>
                                 <input
-                                    id="contact-email"
+                                    name="email"
+                                    id="email"
                                     type="email"
+                                    value={formData.email}
                                     placeholder="Email Address"
                                     className="w-full px-4 py-2 rounded-lg bg-white/80 text-black focus:outline-none focus:ring-2 focus:ring-blue-400"
+                                    required
+                                    onChange={handleChange}
                                 />
                             </div>
                          </div>
@@ -30,15 +105,21 @@ function Contact(){
               {/*detail text area*/}
             </div>
                         <div className="w-[70%] mb-10 mt-10 max-w-[1600px]">
-                            <label htmlFor="contact-details" className="block text-left text-white text-xl 2xl:text-2xl font-poppins mb-1 pl-1">Details:</label>
+                            <label htmlFor="message" className="block text-left text-white text-xl 2xl:text-2xl font-poppins mb-1 pl-1">Details:</label>
                             <textarea
-                                id="contact-details"
+                                name="message"
+                                id="message"
+                                value={formData.message}
                                 placeholder="Please provide as much information as you can about the work and services you need, such as your location, property type, number of windows etc."
                                 className="min-h-[300px] w-full px-4 py-2 rounded-lg bg-white/80 text-black focus:outline-none focus:ring-2 focus:ring-blue-400 resize-y align-top placeholder:text-left"
                                 style={{resize: 'vertical'}}
+                                required
+                                onChange={handleChange}
                             />
                         </div>
-            <a href="#contact" className="bg-[#26578F] mb-8 text-white px-15 lg:px-25 py-5 rounded-full text-md md:text-lg lg:text-xl text-nowrap  font-poppins font-semibold hover:bg-blue-700 transition duration-300">SUBMIT</a>
+                         <button type="submit"  disabled={isSubmitting} className="bg-[#26578F] cursor-pointer mb-8 text-white px-15 lg:px-25 py-5 rounded-full text-md md:text-lg lg:text-xl text-nowrap  font-poppins font-semibold hover:bg-blue-700 transition duration-300">{isSubmitting ? "SENDING..." : "SUBMIT"}</button>
+                        </form>
+           
     </div>);
 
 }
--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -6,7 +6,7 @@ function Home() {
       <div className=" bg-opacity-80 p-10 rounded-lg text-center max-w-[1920px] sm:max-w-[60%] mt-[15%] mx-4 z-10">
         <h1 className="text-[clamp(1.5rem,3.5vw,4rem)] font-poppins font-extrabold mb-6 bg-clip-text text-transparent bg-gradient-to-b from-white from-70% to-gray-400">PROFFESSIONAL WINDOW CLEANING IN YOUR LOCAL AREA</h1>
         <p className="text-md md:text-xl lg:text-2xl font-poppins mb-15 text-white">STREAK-FREE WINDOWS, SPARKLING CONSERVATORIES. YOUR HOME AT ITS BEST.</p>
-        <a href="#contact" className="bg-[#26578F] text-white px-3 lg:px-10 py-5 rounded-full text-md md:text-lg lg:text-xl text-nowrap  font-poppins font-semibold hover:bg-blue-700 transition duration-300">CONTACT US NOW!</a>
+        <a href="#contact" className="bg-[#26578F] cursor-pointer text-white px-3 lg:px-10 py-5 rounded-full text-md md:text-lg lg:text-xl text-nowrap  font-poppins font-semibold hover:bg-blue-700 transition duration-300">CONTACT US NOW!</a>
       </div>
     </div>
   );
